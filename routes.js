a369fbf95c861563eaa500d331493098f4c965f0
Kaike Carmona <kkaike371@gmail.com>
2024-02-14 15:20:09 -0300

    correção dos nomes das pastas

--- routes.js
+++ routes.js
@@ -1,28 +1,16 @@
-const Tarefa = require("./tarefa.js");
+const express = require("express");
+const routes = require("./controller.js");
+const db = require("./db.js");
+const tarefa = require("./tarefa.js");
 
+const app = express();
+app.use(express.json());
+app.use(routes);
 
-async function findUsers(req, res) {
-  Tarefa.findAll()
-    .then((result) => res.json(result))
-    .catch((error) => {
-      console.error("Erro ao buscar tarefas:", error);
-      res.status(500).json({ error: "Erro interno do servidor" });
-    });
-}
-async function creatUser(req, res) {
-  try {
-    const { nome_Tarefa, desc_tarefa, data_finalizacao } = req.body;
-    const novaTarefa = await Tarefa.create({
-      nome_Tarefa,
-      desc_tarefa,
-      data_finalizacao,
-    });
-    res.status(201).json(novaTarefa);
-  } catch (error) {
-    console.error("Erro ao criar nova tarefa:", error);
-    res.status(500).json({ error: "Erro interno do servidor" });
-  }
-}
+app.get("/users", tarefa.findAll);
+app.post("/post", function () {
+  tarefa.post;
+});
 
-//erro ao esportar mais de uma função//
-module.exports = findUsers;
+db.sync(() => console.log(`Banco de dados conectado: `));
+app.listen(3000, () => console.log("Servidor iniciado na porta 3000"));
